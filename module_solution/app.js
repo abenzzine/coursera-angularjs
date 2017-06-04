fe069b4852bed515778f682ac0933362701442db
Abdellatif Benzzine <abdellatifbenzzine@Abdellatifs-MacBook-Pro.local>
2017-06-04 01:34:34 -0500

    Changed module name as per assignment notes

---
+++ module_solution/app.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+    angular.module('MyApp', [])
+        .controller('MyController', MyController);
+    MyController.$inject = ['$scope'];
+
+    function MyController($scope) {
+        $scope.lunchList = "";
+        $scope.message = "";
+        $scope.helpMessage = "";
+
+        function setAlertStyle(color) {
+            $scope.inputStyle = {
+                'border-color': color
+            };
+            $scope.mesageStyle = {
+                'color': color
+            };
+        }
+        $scope.check = function() {
+            var lunchList = $scope.lunchList;
+            if (lunchList.length > 0) {
+                var count = 0;
+                var lunchArr = lunchList.split(",");
+                var lunchArrSize = lunchArr.length;
+                for (var i = 0; i < lunchArrSize; i++) {
+                    if (lunchArr[i].trim().length > 0) {
+                        count++;
+                    }
+                }
+                setAlertStyle('#398439');
+                $scope.message = count > 3 ? "Too much!" : "Enjoy!";
+                $scope.helpMessage = count < lunchArrSize ? "I don't consider empty item!" : "";
+            } else {
+                setAlertStyle('#B5555A');
+                $scope.message = "Please enter the items list data first";
+            }
+        };
+    }
+})();
